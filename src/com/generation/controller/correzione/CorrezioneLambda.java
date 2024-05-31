8aa09bfed66ab641c1104a83ed700b5766650eea
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-05-31 10:54:58 +0200

    v2

--- src/com/generation/controller/correzione/CorrezioneLambda.java
+++ src/com/generation/controller/correzione/CorrezioneLambda.java
@@ -1,12 +1,20 @@
 package com.generation.controller.correzione;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import com.generation.controller.BaseHandler;
+import com.generation.model.Batch;
 import com.generation.model.Category;
 import com.generation.model.Client;
+import com.generation.model.Contract;
+import com.generation.model.Employee;
 import com.generation.model.Product;
 
 public class CorrezioneLambda extends BaseHandler
@@ -41,6 +49,7 @@ public class CorrezioneLambda extends BaseHandler
         System.out.println(res);
     }
 
+
     public void es3()
     {
         //Come mai partiamo da Category e non da Product
@@ -50,11 +59,157 @@ public class CorrezioneLambda extends BaseHandler
 
         //Dopo aver raggruppato posso scegliere di fare una RIDUZIONE, vale a dire trasformare la lista di entità in un unico valore
 
-        Map<String,Long> numProdCat =    categories         //devo passare da una Lista di Categories ad uno Stream<Product>
+        //con questo calcolo ad esempio il peso totale
+        Map<String,Integer> numProdCat =    categories         //devo passare da una Lista di Categories ad uno Stream<Product>
                                             .stream()
                                             .flatMap(c -> c.getProducts().stream())
-                                            .collect(Collectors.groupingBy(p -> p.getCategory().getName(),Collectors.counting()));
+                                            .collect(
+                                                        Collectors.groupingBy(
+                                                                                p -> p.getCategory().getName(),
+                                                                                Collectors.summingInt(p -> p.getWeight())
+                                                                             )
+                                                    );
 
         
+        Map<String,Integer> res =   categories
+                                    .stream()
+                                    .collect(Collectors.toMap(c -> c.getName(), c -> c.getProducts().size()));
     }
+
+    public void es4()
+    {
+        List<Product> products = db.selectAllProducts();
+        List<String> res =  products
+                            .stream()
+                            .filter(p -> p.getWeight()>350)
+                            .map(p -> p.getName() + " "+p.getDescription()+ " "+ (p.getWeight()*1000) +" gr")
+                            .collect(Collectors.toList());
+    }
+
+    
+    public void es5()
+    {
+        List<Product> products = db.selectAllProducts();
+
+        Map<String,Double> res =    products
+                                    .stream()
+                                    .flatMap(p -> p.getReviews().stream())
+                                    .collect(
+                                                Collectors.groupingBy(
+                                                                        r -> r.getProduct().getName(),
+                                                                        Collectors.averagingDouble(r->r.getScore())
+                                                                      )
+                                            );
+
+    }
+
+    public void es6()
+    {
+        List<Contract> contracts = db.selectAllContracts();
+
+        Map<Integer,Long> contrattiPerMese =    contracts
+                                                .stream()
+                                                .filter(c -> c.getAcceptedOn().getYear()==2024)
+                                                .collect(
+                                                            Collectors.groupingBy(
+                                                                                    c-> c.getAcceptedOn().getMonthValue(),
+                                                                                    Collectors.counting()
+                                                            )
+                                                );
+    }
+
+    public void es7()
+    {
+        List<Employee> employees = db.selectAllEmployees();
+
+        List<String> res =      employees
+                                .stream()
+                                .filter(p -> p.getProducts().size()>0)
+                                .map(p -> p.getName() +" "+p.getSurname() + " ha prodotto "+p.getProducts().size()+" prodotti")
+                                .collect(Collectors.toList());
+    }
+
+    public void es8()
+    {
+        List<Product> products = db.selectAllProducts();
+
+        // Map<String,String> res =  products
+        //                                 .stream()
+        //                                 .flatMap(p -> p.getContracts().stream())
+        //                                 .flatMap(c -> c.getBatches().stream())
+        //                                 .filter(b -> b.getStatus().equals("completed"))
+        //                                 .collect(
+        //                                             Collectors.groupingBy(
+        //                                                 b -> b.getContract().getProduct().getName(),
+        //                                                 Collectors.collectingAndThen(
+        //                                                 Collectors.toList(), 
+        //                                                 l ->
+        //                                                 {
+        //                                                     int totaleProdotto = ((Collection<Batch>) l).stream().collect(Collectors.summingInt(b -> b.getUnitsProduced()));
+        //                                                     int totaleScartata =  ((Collection<Batch>) l).stream().collect(Collectors.summingInt(b -> b.getUnityDiscarded()));
+        //                                                     int totalePercScart =  totaleScartata/(totaleProdotto+totaleScartata) *100;
+        //                                                     String fin = "Ho "+totaleProdotto+" pezzi prodotti, "+totaleScartata+" pezzi scartati, con una percentuale di scarto del "+totalePercScart+"%";
+        //                                                     return fin;                                                         
+        //                                                 }
+        //                                             )
+        //                                         )
+        //                                 );
+        Map<String,String> toPrint = new HashMap<>();
+
+        products
+        .stream()
+        .flatMap(p -> p.getContracts().stream())
+        .flatMap(c -> c.getBatches().stream())
+        .filter(b -> b.getStatus().equalsIgnoreCase("completed"))
+        .collect(Collectors.groupingBy( b -> b.getContract().getProduct().getName()))
+        .entrySet()
+        .forEach(
+                    entry -> 
+                    {
+                        List<Batch> batches = entry.getValue();
+                        int totaleProdotto = batches.stream().collect(Collectors.summingInt(b -> b.getUnitsProduced()));
+                        int totaleScartata = batches.stream().collect(Collectors.summingInt(b -> b.getUnityDiscarded()));
+                        int totalePercScart =  totaleScartata/(totaleProdotto+totaleScartata) *100;
+                        String fin = "Ho "+totaleProdotto+" pezzi prodotti, "+totaleScartata+" pezzi scartati, con una percentuale di scarto del "+totalePercScart+"%";
+                        toPrint.put(entry.getKey(), fin);
+                    }
+                );
+       
+
+
+        
+
+        for(Entry<String,List<Batch>> entry : res.entrySet())
+        {
+            List<Batch> batches = entry.getValue();
+            int totaleProdotto = batches.stream().collect(Collectors.summingInt(b -> b.getUnitsProduced()));
+            int totaleScartata = batches.stream().collect(Collectors.summingInt(b -> b.getUnityDiscarded()));
+            int totalePercScart =  totaleScartata/(totaleProdotto+totaleScartata) *100;
+            String fin = "Ho "+totaleProdotto+" pezzi prodotti, "+totaleScartata+" pezzi scartati, con una percentuale di scarto del "+totalePercScart+"%";
+            toPrint.put(entry.getKey(), fin);
+        }
+
+
+        System.out.println(toPrint);
+
+     
+
+        List<Batch> batches2 =  products
+                                .stream()
+                                .flatMap(p -> p.getContracts().stream())
+                                .flatMap(c -> c.getBatches().stream())
+                                .filter(b -> b.getStatus().equals("completed"))
+                                .collect(Collectors.toList());
+
+           //soluzione senza Stream
+        List<Batch> batches =  new ArrayList<>();
+
+
+        for(Product p : products)
+            for(Contract c : p.getContracts())
+                for(Batch b: c.getBatches())
+                    if(b.getStatus().equalsIgnoreCase("completed"))
+                        batches.add(b);
+    }
+
 }
